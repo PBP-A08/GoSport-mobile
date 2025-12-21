9e3f84c23ec1d293db2c064b969ced6291cfe6d8
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2025-12-21 16:53:24 +0700

    Add pay, complete, and delete buttons to transaction details page

--- lib/screens/payment/transaction_detail.dart
+++ lib/screens/payment/transaction_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gosport_mobile/models/transaction.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
@@ -54,6 +55,152 @@ class _TransactionDetailPageState extends State<TransactionDetailPage> {
 
   }
 
+  void showPayDialog() async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // TODO: Validate transaction status and credentials first
+        final controller = TextEditingController();
+        return AlertDialog(
+          title: const Text('Add payment'),
+          content: TextField(
+            controller: controller,
+            keyboardType: TextInputType.number,
+            inputFormatters: [
+              FilteringTextInputFormatter.digitsOnly,
+            ],
+            decoration: InputDecoration(
+              labelText: 'Enter payment amount',
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                final amount = double.tryParse(controller.text);
+                if (amount != null) {
+                  pay(transactionId, amount);
+                }
+                Navigator.of(context).pop();
+              },
+              child: const Text('Confirm'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void pay(String id, double amount) async {
+    final request = context.read<CookieRequest>();
+    try {
+      final response = await request.post(
+          Urls.paymentPay.replaceFirst('<uuid:id>', id), {
+            'pay-amount': "$amount"});
+      // TODO: Error handling
+      // print(response);
+    } catch (e) {
+      print(e);
+    }
+    fetchTransaction();
+  }
+
+  void showCompleteDialog() async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // TODO: Validate transaction status and credentials first
+        return AlertDialog(
+          title: const Text('Complete transaction?'),
+          content: const Text('Are you sure you want to complete this transaction?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                complete(transactionId);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void complete(String id) async {
+    final request = context.read<CookieRequest>();
+    try {
+      final response = await request.post(
+          Urls.paymentComplete.replaceFirst('<uuid:id>', id), {});
+      // TODO: Error handling
+      // print(response);
+    } catch (e) {
+      print(e);
+    }
+    fetchTransaction();
+  }
+
+  void showDeleteDialog() async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context2) {
+        // TODO: Validate transaction status and credentials first
+        return AlertDialog(
+          title: const Text('Complete transaction?'),
+          content: const Text('Are you sure you want to delete this transaction?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context2).pop();
+              },
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.of(context2).pop();
+                if (await delete(transactionId)) {
+                  // Pop current page
+                  if (mounted) {
+                    Navigator.of(context).pop();
+                  }
+                }
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<bool> delete(String id) async {
+    final request = context.read<CookieRequest>();
+    try {
+      final response = await request.post(
+          Urls.paymentDelete.replaceFirst('<uuid:id>', id), {});
+      // TODO: Error handling
+      print(response);
+      if (response["status"] == 'success') {
+        return true;
+      }
+    } catch (e) {
+      print(e);
+    }
+    fetchTransaction();
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -84,6 +231,11 @@ class _TransactionDetailPageState extends State<TransactionDetailPage> {
 
     }
 
+    final (statusIcon, statusLine, statusColor) = transaction.paymentStatus == PaymentStatus.complete
+        ? (Icons.check, "Completed", Colors.green[900])
+        : transaction.amountDue <= 0
+        ? (Icons.pending_actions, "Fully paid, awaiting completion", Colors.blue[900])
+        : (Icons.attach_money, "Not fully paid yet", Colors.yellow[900]);
     return Scaffold(
       appBar: AppBar(title: const Text("Transaction Details")),
       body: SingleChildScrollView(
@@ -99,6 +251,17 @@ class _TransactionDetailPageState extends State<TransactionDetailPage> {
               elevation: 2,
               child: Column(
                 children: [
+                  ListTile(
+                    leading: Icon(statusIcon),
+                    title: const Text("Status"),
+                    subtitle: Text(
+                      statusLine,
+                      style: TextStyle(
+                        color: statusColor,
+                      )
+                    ),
+                  ),
+                  const Divider(height: 0),
                   ListTile(
                     leading: const Icon(Icons.calendar_today),
                     title: const Text("Created at"),
@@ -120,6 +283,29 @@ class _TransactionDetailPageState extends State<TransactionDetailPage> {
                     title: const Text("Total Items"),
                     subtitle: Text("${transaction.itemCount} items"),
                   ),
+                  const Divider(height: 0),
+                  ListTile(
+                    leading: const Icon(Icons.attach_money),
+                    title: const Text("Total price"),
+                    subtitle: Text("Rp ${transaction.totalPrice}"),
+                  ),
+                  const Divider(height: 0),
+                  ListTile(
+                    leading: const Icon(Icons.paid),
+                    title: const Text("Amount paid"),
+                    subtitle: Text("Rp ${transaction.amountPaid}"),
+                  ),
+                  const Divider(height: 0),
+                  ListTile(
+                    leading: const Icon(Icons.upload),
+                    title: const Text("Amount due"),
+                    subtitle: Text(
+                      "Rp ${transaction.amountDue}",
+                      style: TextStyle(
+                        color: transaction.amountDue > 0 ? Colors.orange : Colors.green,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -130,15 +316,15 @@ class _TransactionDetailPageState extends State<TransactionDetailPage> {
             Row(
               children: [
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: showPayDialog,
                   child: Text("Pay"),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: showCompleteDialog,
                   child: Text("Complete"),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: showDeleteDialog,
                   child: Text("Delete"),
                 ),
               ],
